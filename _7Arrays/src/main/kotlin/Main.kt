64bcb0f37539561196413e7c31fc8dd00645960c
ahmed.ganna <ahmed.ganna@check24.de>
2025-02-03 23:12:41 +0100

    Arrays

---
+++ _7Arrays/src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val arr = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    print(arr.reversed().joinToString(" "))
+}
